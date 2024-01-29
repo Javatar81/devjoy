9d98e37e8aec84294ccd6b0f60c717e720e3d2bd
Javatar81 <benjamin.schmeling@gmail.com>
2024-01-29 23:31:38 +0100

    Set version in CSV to 0.2.0-SNAPSHOT

--- operators/gitea/gitea-operator/src/main/java/io/devjoy/gitea/k8s/GiteaReconciler.java
+++ operators/gitea/gitea-operator/src/main/java/io/devjoy/gitea/k8s/GiteaReconciler.java
@@ -76,7 +76,7 @@ import jakarta.ws.rs.WebApplicationException;
 		@Dependent(type = KeycloakClientDependentResource.class, activationCondition = KeycloakReconcileCondition.class) 
 		
 })
-@CSVMetadata(name = "gitea-operator-bundle", version = "0.1.0", displayName = "Gitea Operator", description = "An operator to manage Gitea servers and repositories", provider = @Provider(name = "devjoy.io"), keywords = "Git,Repository,Gitea", annotations = @Annotations(repository = "https://github.com/Javatar81/devjoy", containerImage = "quay.io/devjoy/gitea-operator:0.1.0", others= {}))
+@CSVMetadata(name = "gitea-operator-bundle", version = "0.2.0-SNAPSHOT", displayName = "Gitea Operator", description = "An operator to manage Gitea servers and repositories", provider = @Provider(name = "devjoy.io"), keywords = "Git,Repository,Gitea", annotations = @Annotations(repository = "https://github.com/Javatar81/devjoy", containerImage = "quay.io/devjoy/gitea-operator:0.2.0-SNAPSHOT", others= {}))
 public class GiteaReconciler implements Reconciler<Gitea>, ErrorStatusHandler<Gitea>, SharedCSVMetadata { 
 	
 	private static final String GITEA_TRUST_BUNDLE_MAP_NAME = "gitea-trust-bundle";
