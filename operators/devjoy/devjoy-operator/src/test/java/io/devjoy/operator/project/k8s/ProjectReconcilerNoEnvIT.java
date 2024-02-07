db7734668d8f7e1baa8a351d9bcbb05a075bffe3
Javatar81 <benjamin.schmeling@gmail.com>
2024-02-07 17:51:33 +0100

    Different namespaces for operator and cr.

--- operators/devjoy/devjoy-operator/src/test/java/io/devjoy/operator/project/k8s/ProjectReconcilerNoEnvIT.java
+++ operators/devjoy/devjoy-operator/src/test/java/io/devjoy/operator/project/k8s/ProjectReconcilerNoEnvIT.java
@@ -10,24 +10,25 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.openshift.client.OpenShiftClient;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 
 @QuarkusTest
 public class ProjectReconcilerNoEnvIT {
-    @Inject
-	OpenShiftClient client;
+   
+    static OpenShiftClient client = new KubernetesClientBuilder().build().adapt(OpenShiftClient.class);
 
     @AfterEach
 	void tearDown() {
-        client.resources(Project.class).delete();
+        client.resources(Project.class).inNamespace(getTargetNamespace()).delete();
 	}
 
     @Test
     public void testCreateProject() {
         Project project = new Project();
-        project.getMetadata().setNamespace(client.getNamespace());
+        project.getMetadata().setNamespace(getTargetNamespace());
         project.getMetadata().setName("noenvproj");
         ProjectSpec spec = new ProjectSpec();
         ProjectOwner owner = new ProjectOwner();
@@ -41,11 +42,15 @@ public class ProjectReconcilerNoEnvIT {
         project.setSpec(spec);
         client.resource(project).create();
         await().ignoreException(NullPointerException.class).atMost(200, TimeUnit.SECONDS).untilAsserted(() -> {
-            final var projectResource = client.resources(Project.class).inNamespace(client.getNamespace()).withName(project.getMetadata().getName()).get();
+            final var projectResource = client.resources(Project.class).inNamespace(getTargetNamespace()).withName(project.getMetadata().getName()).get();
             assertThat(projectResource.getStatus().getConditions().stream().anyMatch(c -> ProjectConditionType.ENV_NOT_FOUND.toString().equals(c.getType())), is(true));
 
         });
     }
+
+    private static String getTargetNamespace() {
+		return client.getNamespace() + "2";
+	}
 
 
 }
