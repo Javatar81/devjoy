eee178dfca394b098c98534ba2919a9787e7f4d0
Javatar81 <benjamin.schmeling@gmail.com>
2024-02-08 07:54:33 +0100

    Set registerFullHierarchy=true to address #59 Operator does not recognize observedGeneration field in native mode

--- operators/gitea/gitea-operator/src/test/java/io/devjoy/gitea/k8s/GiteaRepositoryReconcilerIT.java
+++ operators/gitea/gitea-operator/src/test/java/io/devjoy/gitea/k8s/GiteaRepositoryReconcilerIT.java
@@ -26,7 +26,7 @@ import io.quarkus.test.junit.QuarkusTest;
 
 
 @QuarkusTest
-class GiteaRepositoryReconcilerIT {
+public class GiteaRepositoryReconcilerIT {
 
 	static OpenShiftClient client = new KubernetesClientBuilder().build().adapt(OpenShiftClient.class);
 	static TestEnvironment env = new TestEnvironment(client, ConfigProvider.getConfig().getOptionalValue("test.quarkus.kubernetes-client.devservices.flavor", String.class));
--- operators/gitea/gitea-operator/src/test/java/io/devjoy/gitea/k8s/integration/GiteaRepositoryReconcilerNativeIT.java
+++ operators/gitea/gitea-operator/src/test/java/io/devjoy/gitea/k8s/integration/GiteaRepositoryReconcilerNativeIT.java
@@ -1,9 +1,9 @@
 package io.devjoy.gitea.k8s.integration;
 
-import io.devjoy.gitea.k8s.GiteaReconcilerIT;
+import io.devjoy.gitea.k8s.GiteaRepositoryReconcilerIT;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class GiteaRepositoryReconcilerNativeIT extends GiteaReconcilerIT{
+public class GiteaRepositoryReconcilerNativeIT extends GiteaRepositoryReconcilerIT{
     
 }
