baffadca3caf589220f1fa905b84b1bda29aaca5
Javatar81 <benjamin.schmeling@gmail.com>
2024-07-31 16:59:37 +0200

    Added native integration test for organization reconciler

---
+++ operators/gitea/gitea-operator/src/test/java/io/devjoy/gitea/k8s/integration/GiteaOrganizationReconcilerNativeIT.java
@@ -0,0 +1,9 @@
+package io.devjoy.gitea.k8s.integration;
+
+import io.devjoy.gitea.k8s.GiteaOrganizationReconcilerIT;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GiteaOrganizationReconcilerNativeIT extends GiteaOrganizationReconcilerIT{
+
+}
