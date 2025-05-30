affb14b11f40d6bd13036fe63d308370b8decd24
Javatar81 <benjamin.schmeling@gmail.com>
2025-05-30 15:46:31 +0200

    Constants for version and group

--- operators/gitea/gitea-operator-model/src/main/java/io/devjoy/gitea/k8s/model/Gitea.java
+++ operators/gitea/gitea-operator-model/src/main/java/io/devjoy/gitea/k8s/model/Gitea.java
@@ -5,10 +5,11 @@ import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.Version;
 
-@Version("v1alpha1")
-@Group("devjoy.io")
+@Version(Gitea.API_VERSION)
+@Group(Gitea.API_GROUP)
 public class Gitea extends CustomResource<GiteaSpec, GiteaStatus> implements Namespaced {
-
+	public static final String API_GROUP = "devjoy.io";
+	public static final String API_VERSION = "v1alpha1";
 	private static final long serialVersionUID = -6663671542664201741L;
 	
 }
